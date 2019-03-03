77725c1ace21c171adc9ba4f5f29fceb04d7f45b
amikaila <arturas.lbs@gmail.com>
2019-03-03 21:56:35 +0200

    Init commit

--- IntOps.java
+++ IntOps.java
@@ -1,5 +1,5 @@
 public class IntOps {
-  public static void main(String[] args){
+  public static void main(String[] args) {
     int a = Integer.parseInt(args[0]);
     int b = Integer.parseInt(args[1]);
     int p = a * b;
